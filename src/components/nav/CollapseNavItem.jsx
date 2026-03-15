34e0a292441de1e8d326274b3e6fa22ff9f7fe02
Cayden <18541769+ClearlyRubix@users.noreply.github.com>
2026-03-15 17:24:53 -0600

    mobile navbar

---
+++ src/components/nav/CollapseNavItem.jsx
@@ -0,0 +1,25 @@
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { Collapse, List, ListItemButton, ListItemText } from "@mui/material";
+import { useState } from "react";
+
+const CollapseNavItem = ({ buttonText = "", items = [] }) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <ListItemButton onClick={() => {setOpen((prev) => !prev)}}>
+                <ListItemText primary={buttonText} />
+                {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout={"auto"}>
+                <List component={"div"} disablePadding sx={{ pl: 2 }}>
+                    {items.map((item, i) => (
+                        <span key={i}>{item}</span>
+                    ))}
+                </List>
+            </Collapse>
+        </>
+    );
+};
+
+export default CollapseNavItem;
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,10 @@
 // TODO: Create mobile nav bar
 
 // Package Imports
-import { AppBar, Toolbar, CssBaseline, useTheme, Avatar } from "@mui/material";
+import { AppBar, Toolbar, CssBaseline, useTheme, Avatar, Box, IconButton, SwipeableDrawer, List, ListItemButton, ListItemText } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu"
+import { useState } from "react";
+
 
 // Project-defined Imports
 import {
@@ -11,16 +14,28 @@ import {
 } from "./menu-items";
 import NonPopoverNavItem from "./NonPopoverNavItem";
 import NavMenuPopover from "./popover/NavMenuPopover";
+import CollapseNavItem from "./CollapseNavItem";
 
 // Images
 import ZooLogo from "../../assets/ZooLogo.svg";
+import { Link } from "react-router";
+
+
+const clickScroll = (id) => {
+  const el = document.getElementById(id);
+  if (el) el.scrollIntoView({ behavior: "smooth", block: "start" });
+};
 
 export const Navbar = ({ isRecruiting }) => {
   const theme = useTheme();
   const variant = "body1";
 
+  const [menuOpen, setMenuOpen] = useState(false)
+
+
   return (
     <AppBar
+      id="navbar"
       position="static"
       sx={{
         backgroundColor: theme.palette.blue.primary,
@@ -40,7 +55,50 @@ export const Navbar = ({ isRecruiting }) => {
         >
           <Avatar alt="Zoo Logo" src={ZooLogo} />
         </a>
-        <div style={{ display: "flex" }}>
+          
+        {/* Mobile Navbar */}
+        <Box sx={{
+          display: {xs: "flex", md: "none"}
+        }}>
+          <IconButton sx={{
+            ".MuiSvgIcon-root": {
+              color: theme.palette.white.primary,
+            },
+          }}
+          onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <SwipeableDrawer
+          anchor={"right"}
+          open={menuOpen}
+          onOpen={() => setMenuOpen(true)}
+          onClose={() => setMenuOpen(false)}
+          disableSwipeToOpen={false}
+          disableScrollLock={true}
+          >
+            <List>
+              <ListItemButton component={Link} to={"/#about-zoo"} onClick={() => setMenuOpen(false)}>
+                <ListItemText primary="About"/>
+              </ListItemButton>
+              <CollapseNavItem buttonText="Council" items={[
+                <CouncilNavMenuItems />
+              ]}/>
+              <CollapseNavItem buttonText="Events" items={[
+                <EventNavMenuItems />
+              ]}/>
+              <CollapseNavItem buttonText="Sponsors" items={[
+                <SponsorNavMenuItems />
+              ]}/>
+              <ListItemButton component={Link} to={"/#contact-us"} onClick={() => setMenuOpen(false)}>
+                <ListItemText primary="Contact Us"/>
+              </ListItemButton>
+            </List>
+
+          </SwipeableDrawer>
+        </Box>
+        {/* Desktop Navbar */}
+        <Box sx={{ display: {xs: "none", md: "flex"} }}>
           <NonPopoverNavItem
             variant={variant}
             label="About"
@@ -79,7 +137,7 @@ export const Navbar = ({ isRecruiting }) => {
             label="Contact Us "
             href="/#contact-us"
           />
-        </div>
+        </Box>
       </Toolbar>
     </AppBar>
   );
