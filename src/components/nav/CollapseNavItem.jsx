9fb4f250bf0d230cf931ea3f4568b8bcb829759a
Cayden <18541769+ClearlyRubix@users.noreply.github.com>
2026-03-15 17:25:01 -0600

    mobile navbar

--- src/components/nav/CollapseNavItem.jsx
+++ src/components/nav/CollapseNavItem.jsx
@@ -1,14 +1,20 @@
 import { ExpandLess, ExpandMore } from "@mui/icons-material";
-import { Collapse, List, ListItemButton, ListItemText } from "@mui/material";
+import { Collapse, List, ListItemButton, ListItemText, useTheme } from "@mui/material";
 import { useState } from "react";
 
 const CollapseNavItem = ({ buttonText = "", items = [] }) => {
     const [open, setOpen] = useState(false);
-
+    const theme = useTheme()
     return (
         <>
-            <ListItemButton onClick={() => {setOpen((prev) => !prev)}}>
-                <ListItemText primary={buttonText} />
+            <ListItemButton
+                onClick={() => {
+                    setOpen((prev) => !prev);
+                }}
+            >
+                <ListItemText
+                    primary={buttonText}
+                />
                 {open ? <ExpandLess /> : <ExpandMore />}
             </ListItemButton>
             <Collapse in={open} timeout={"auto"}>
