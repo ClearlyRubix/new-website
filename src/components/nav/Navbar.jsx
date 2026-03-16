b00cb8e7151f8daf003566438e04211bafbd762f
Cayden <18541769+ClearlyRubix@users.noreply.github.com>
2026-03-15 18:38:06 -0600

    mobile navbar similar to current site

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,7 @@
 // TODO: Create mobile nav bar
 
 // Package Imports
-import { AppBar, Toolbar, CssBaseline, useTheme, Avatar, Box, IconButton, SwipeableDrawer, List, ListItemButton, ListItemText } from "@mui/material";
+import { AppBar, Toolbar, CssBaseline, useTheme, Avatar, Box, IconButton, Collapse, List, ListItemButton, ListItemText, Fade } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu"
 import { useState } from "react";
 
@@ -28,119 +28,149 @@ export const Navbar = ({ isRecruiting }) => {
 
 
   return (
-    <AppBar
-      id="navbar"
-      position="static"
-      sx={{
-        backgroundColor: theme.palette.blue.primary,
-        height: {xs: "56px", sm: "64px"}
-      }}
-    >
-      <CssBaseline />
-      <Toolbar
+    <>
+      {/* Backdrop */}
+      <Fade in={menuOpen} timeout={250}>
+        <Box
+          position={"fixed"}
+          top={0}
+          left={0}
+          bottom={0}
+          right={0}
+          zIndex={1200}
+          width={"100vw"}
+          height={"100vh"}
+          backgroundColor={"rgba(0,0,0,0.5)"}
+          onClick={() => setMenuOpen(false)}
+        />
+      </Fade>
+      <AppBar
+        id="navbar"
+        position="relative"
         sx={{
-          justifyContent: "space-between",
+          backgroundColor: theme.palette.blue.primary,
+          height: {xs: "56px", sm: "64px"},
+          width: "100vw",
+          zIndex: 1200
         }}
       >
-        <a
-          href={"/meaning-of-zoo"}
-          style={{
-            textDecoration: "none",
+        <CssBaseline />
+        <Toolbar
+          sx={{
+            justifyContent: "space-between",
+            width: "100%",
           }}
         >
-          <Avatar alt="Zoo Logo" src={ZooLogo} />
-        </a>
-          
-        {/* Mobile Navbar */}
-        <Box sx={{
-          display: {xs: "flex", md: "none"}
-        }}>
-          <IconButton sx={{
-            ".MuiSvgIcon-root": {
-              color: theme.palette.white.primary,
-            },
-          }}
-          onClick={() => setMenuOpen(!menuOpen)}
+          <Link
+            to={"/meaning-of-zoo"}
+            style={{
+              textDecoration: "none",
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <SwipeableDrawer
-          anchor={"right"}
-          open={menuOpen}
-          onOpen={() => setMenuOpen(true)}
-          onClose={() => setMenuOpen(false)}
-          disableSwipeToOpen={false}
-          disableScrollLock={true}
-          PaperProps={{
-            sx: {
-              backgroundColor: theme.palette.blue.primary,
-              color: theme.palette.white.primary
-            }
-          }}
+            <Avatar alt="Zoo Logo" src={ZooLogo} />
+          </Link>
+            
+          {/* Mobile Navbar */}
+          <Box
+            sx={{display: {xs: "flex", md: "none"}, flexDirection: "column"}}
+            height={"100%"}
           >
-            <List>
-              <ListItemButton component={Link} to={"/#about-zoo"} onClick={() => setMenuOpen(false)}>
-                <ListItemText primary="About"/>
-              </ListItemButton>
-              <CollapseNavItem buttonText="Council" items={[
-                <CouncilNavMenuItems />
-              ]}/>
-              <CollapseNavItem buttonText="Events" items={[
-                <EventNavMenuItems />
-              ]}/>
-              <CollapseNavItem buttonText="Sponsors" items={[
-                <SponsorNavMenuItems />
-              ]}/>
-              <ListItemButton component={Link} to={"/#contact-us"} onClick={() => setMenuOpen(false)}>
-                <ListItemText primary="Contact Us"/>
-              </ListItemButton>
-            </List>
+            {/* Top Navbar Row */}
+            <Box
+              sx={{ display: "flex", justifyContent: "flex-end"}}
+              height={"100%"}
+            >
+              <IconButton sx={{
+                ".MuiSvgIcon-root": {
+                  color: theme.palette.white.primary,
+                },
+              }}
+              onClick={() => setMenuOpen(!menuOpen)}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            {/* Collapsed Navbar */}
+            <Collapse
+              in={menuOpen}
+              timeout={"auto"}
+              sx={{
+                zIndex: 1200,
+                position: "absolute",
+                top: "100%",
+                right: 0,
+                width: "100vw"
+              }}
+            >
+              <List
+                sx={{
+                  backgroundColor: theme.palette.blue.primary,
+                  zIndex: 1200,
+                }}
+              >
+                <ListItemButton component={Link} to={"/#about-zoo"} onClick={() => setMenuOpen(false)}>
+                  <ListItemText primary="About"/>
+                </ListItemButton>
+                <CollapseNavItem buttonText="Council" items={[
+                  <CouncilNavMenuItems />
+                ]}/>
+                <CollapseNavItem buttonText="Events" items={[
+                  <EventNavMenuItems />
+                ]}/>
+                <CollapseNavItem buttonText="Sponsors" items={[
+                  <SponsorNavMenuItems />
+                ]}/>
+                <ListItemButton component={Link} to={"/#contact-us"} onClick={() => setMenuOpen(false)}>
+                  <ListItemText primary="Contact Us"/>
+                </ListItemButton>
+              </List>
+            </Collapse>
+            
+          </Box>
+          {/* Desktop Navbar */}
+          <Box sx={{ display: {xs: "none", md: "flex"} }} height={"100%"}>
+            <NonPopoverNavItem
+              variant={variant}
+              label="About"
+              href="/#about-zoo"
+            />
 
-          </SwipeableDrawer>
-        </Box>
-        {/* Desktop Navbar */}
-        <Box sx={{ display: {xs: "none", md: "flex"} }} height={"100%"}>
-          <NonPopoverNavItem
-            variant={variant}
-            label="About"
-            href="/#about-zoo"
-          />
+            <NavMenuPopover
+              variant={variant}
+              menuId={"council-menu"}
+              buttonLabel={"Council"}
+              buttonId={"council-nav-button"}
+            >
+              <CouncilNavMenuItems isRecruiting={isRecruiting} />
+            </NavMenuPopover>
 
-          <NavMenuPopover
-            variant={variant}
-            menuId={"council-menu"}
-            buttonLabel={"Council"}
-            buttonId={"council-nav-button"}
-          >
-            <CouncilNavMenuItems isRecruiting={isRecruiting} />
-          </NavMenuPopover>
+            <NavMenuPopover
+              variant={variant}
+              menuId={"events-menu"}
+              buttonLabel={"Events"}
+              buttonId={"events-nav-button"}
+            >
+              <EventNavMenuItems />
+            </NavMenuPopover>
 
-          <NavMenuPopover
-            variant={variant}
-            menuId={"events-menu"}
-            buttonLabel={"Events"}
-            buttonId={"events-nav-button"}
-          >
-            <EventNavMenuItems />
-          </NavMenuPopover>
+            <NavMenuPopover
+              variant={variant}
+              menuId={"sponsor-menu"}
+              buttonLabel={"Sponsors"}
+              buttonId={"sponsor-nav-button"}
+            >
+              <SponsorNavMenuItems />
+            </NavMenuPopover>
 
-          <NavMenuPopover
-            variant={variant}
-            menuId={"sponsor-menu"}
-            buttonLabel={"Sponsors"}
-            buttonId={"sponsor-nav-button"}
-          >
-            <SponsorNavMenuItems />
-          </NavMenuPopover>
-
-          <NonPopoverNavItem
-            variant={variant}
-            label="Contact Us "
-            href="/#contact-us"
-          />
-        </Box>
-      </Toolbar>
-    </AppBar>
+            <NonPopoverNavItem
+              variant={variant}
+              label="Contact Us "
+              href="/#contact-us"
+            />
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 export default Navbar;
