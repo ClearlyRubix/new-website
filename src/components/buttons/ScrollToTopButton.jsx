bdc4708a7c7bb587a9926d059ca7ca3c8abbf84b
Poncho <ineswinner@hotmail.com>
2026-03-17 23:24:10 -0600

    Updating scroll to behaviour to be functional again

--- src/components/buttons/ScrollToTopButton.jsx
+++ src/components/buttons/ScrollToTopButton.jsx
@@ -2,35 +2,29 @@
 import React from "react";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
 import { Box, Fade, Fab, useTheme } from "@mui/material";
+import { useWindowScroll } from "@uidotdev/usehooks";
 
 // Icon Imports
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export const ScrollToTopButton = () => {
   const theme = useTheme();
+  const [{}, scrollTo] = useWindowScroll();
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 100,
   });
 
-  const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector(
-      "#back-to-top-anchor"
-    );
-
-    if (anchor) {
-      anchor.scrollIntoView({
-        block: "center",
-      });
-    }
-  };
-
   return (
     <Fade in={trigger}>
       <Box
-        onClick={handleClick}
+        onClick={() => scrollTo({ left: 0, top: 0, behavior: "smooth" })}
         role="presentation"
-        sx={{ position: "fixed", bottom: theme.spacing(2), right: theme.spacing(2) }}
+        sx={{
+          position: "fixed",
+          bottom: theme.spacing(2),
+          right: theme.spacing(2),
+        }}
       >
         <Fab size="small" aria-label="scroll back to top">
           <KeyboardArrowUpIcon />
