8ecd69b55f996182e8f9880f0413b177b7be09b8
Cayden <18541769+ClearlyRubix@users.noreply.github.com>
2026-03-19 22:09:57 -0600

    moved backdrop into MobileNav

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,6 +1,5 @@
 // Package Imports
-import { AppBar, Toolbar, CssBaseline, useTheme, Avatar, Box, Fade } from "@mui/material";
-import { useState } from "react";
+import { AppBar, Toolbar, CssBaseline, useTheme, Avatar } from "@mui/material";
 import { Link } from "react-router";
 
 // Project-defined Imports
@@ -14,27 +13,8 @@ import ZooLogo from "../../assets/ZooLogo.svg";
 export const Navbar = ({ isRecruiting }) => {
   const theme = useTheme();
 
-  const [menuOpen, setMenuOpen] = useState(false); // controls whether the mobile nav menu is open and controls the backdrop
-
-
-
   return (
-    <>
-      {/* Backdrop for when mobile nav menu is open */}
-      <Fade in={menuOpen} timeout={250}>
-        <Box
-          position={"fixed"}
-          top={0}
-          left={0}
-          bottom={0}
-          right={0}
-          zIndex={1200}
-          width={"100vw"}
-          height={"100vh"}
-          backgroundColor={"rgba(0,0,0,0.5)"}
-          onClick={() => setMenuOpen(false)}
-        />
-      </Fade>
+    <>   
       <AppBar
         id="navbar"
         position="relative"
@@ -42,7 +22,7 @@ export const Navbar = ({ isRecruiting }) => {
           backgroundColor: theme.palette.blue.primary,
           height: {xs: "56px", sm: "64px"},
           width: "100vw",
-          zIndex: 1200
+          zIndex: 1202
         }}
       >
         <CssBaseline />
@@ -62,9 +42,9 @@ export const Navbar = ({ isRecruiting }) => {
           </Link>
             
           {/* Mobile Navbar */}
-          <MobileNav menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileNav />
           {/* Desktop Navbar */}
-          <DesktopNav />
+          <DesktopNav isRecruiting={isRecruiting} />
         </Toolbar>
       </AppBar>
     </>
